d2ce19d819126509da3b747972864bd626949ab4
Lorenzo Murarotto <lnzmrr@gmail.com>
2020-08-29 10:44:18 +0000

    set up linters

--- background.js
+++ background.js
@@ -1,20 +1,16 @@
-const CHROME_OS_UA = "Mozilla/5.0 (X11; CrOS x86_64 13099.85.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.110 Safari/537.36";
+const CHROME_OS_UA =
+  "Mozilla/5.0 (X11; CrOS x86_64 13099.85.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.110 Safari/537.36";
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
-  function (details) {
-    const ua = details.requestHeaders.find(function (header) { return header.name === "User-Agent"; });
+  (details) => {
+    const ua = details.requestHeaders.find((header) => header.name === "User-Agent");
     ua.value = CHROME_OS_UA;
     return { requestHeaders: details.requestHeaders };
   },
 
   {
-    urls: [
-      "https://play.geforcenow.com/*"
-    ]
+    urls: ["https://play.geforcenow.com/*"],
   },
 
-  [
-    "blocking",
-    "requestHeaders"
-  ]
+  ["blocking", "requestHeaders"]
 );
